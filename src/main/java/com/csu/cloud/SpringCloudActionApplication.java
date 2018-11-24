c3f5d817321319b3ad541cf0e74c9938794c625a
Sharp <1697708997@qq.com>
2018-11-24 22:56:31 +0800

    Initial commit

---
+++ src/main/java/com/csu/cloud/SpringCloudActionApplication.java
@@ -0,0 +1,12 @@
+package com.csu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudActionApplication.class, args);
+    }
+}
